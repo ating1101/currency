36855c49fe5b64fa27e4133550c903b7ebf1dda3
Tim <s1032030@g.ncyu.edu.tw>
2022-05-01 16:37:24 +0800

    [init][開發項目: 新增依賴所需套件、更正 application 路徑]
    1. 新增依賴所需套件、更正 application 路徑

--- currency-backend/currency-web/src/main/java/com/crm/currency/CurrencyApplication.java
+++ currency-backend/currency-web/src/main/java/com/crm/currency/CurrencyApplication.java
@@ -1,16 +1,17 @@
-package com.currency.crm;
+package com.crm.currency;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ServletComponentScan
 public class CurrencyApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(CurrencyApplication.class, args);
     }
-    
-
-
 }
